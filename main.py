1c95249a0cc40541d83bb907759f44fc3024267d
vachana visweswaraiah <Vachana.Visweswaraiah@aiio.de>
2025-11-27 16:49:47 +0100

    Entire workflow

--- main.py
+++ main.py
@@ -1,31 +1,20 @@
-from google import genai
-from google.genai import types
+# main.py
 
-import tools
- 
-class Agent:
-    def __init__(self, model: str,tools: list[dict]):
-        self.model = model
-        self.client = genai.Client()
-        self.contents = []
-        self.tools = tools
- 
-    def run(self, contents: str):
-        self.contents.append({"role": "user", "parts": [{"text": contents}]})
- 
-        config = types.GenerateContentConfig(
-            tools=[types.Tool(function_declarations=[tool["definition"] for tool in self.tools.values()])],
-        )
- 
-        response = self.client.models.generate_content(model=self.model, contents=self.contents, config=config)
-        self.contents.append(response.candidates[0].content)
- 
-        return response
- 
-agent = Agent(model="gemini-3-pro-preview", tools=tools)
- 
-response = agent.run(
-    contents="Can you list my files in the current directory?"
+
+from agent import Agent
+from tools import file_tools
+
+agent = Agent(
+    model="gemini-3-pro-preview",
+    tools=file_tools,
+    system_instruction="You are a helpful Coding Assistant. Respond like you are Linus Torvalds."
 )
-print(response.function_calls)
-# Output: [FunctionCall(name='list_dir', arguments={'directory_path': '.'})]
+
+print("Agent ready. Ask it to check files in this directory.")
+while True:
+    user_input = input("You: ")
+    if user_input.lower() in ['exit', 'quit']:
+        break
+
+    response = agent.run(user_input)
+    print(f"Linus: {response.text}\n")
